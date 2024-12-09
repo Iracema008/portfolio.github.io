17eb06843d5d8ab29ae03551e6ee93fc3dd89ece
Iracema <110850107+Iracema008@users.noreply.github.com>
2024-12-08 22:39:53 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import App from "./App";
+import { Hero } from "./components/Hero/Hero";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
 
+import "./index.css";
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <Hero />
+    <About />
+    <Experience />
+    <Projects />
+    <Contact />
+  
+    </React.StrictMode>
 );
